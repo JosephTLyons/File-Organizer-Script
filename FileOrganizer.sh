286c6921a2c468b781c1aab71afe10e59a127249
JosephTLyons <josephlyons@josephs-air.pk5001z>
2017-01-20 04:17:26 -0500

    Organizing

--- FileOrganizer.sh
+++ FileOrganizer.sh
@@ -6,46 +6,47 @@
 
 # Make folders for documents
 mkdir Documents
-mkdir Documents/TXT
-mkdir Documents/RTF
 mkdir Documents/DOC
 mkdir Documents/HTML
+mkdir Documents/RTF
+mkdir Documents/TXT
+
 
 # Move all docs to Documents Folder
-mv *.txt Documents/TXT
-mv *.rtf Documents/RTF
-mv *.doc Documents/DOC
-mv *.html Documents/HTML
+mv *.txt Documents/DOC
+mv *.rtf Documents/HTML
+mv *.doc Documents/RTF
+mv *.html Documents/TXT
 
 # Make folders for images
 mkdir Images
+mkdir Images/GIF
 mkdir Images/JPEG
 mkdir Images/JPG
-mkdir Images/GIF
 mkdir Images/PNG
 
 # Move all Images to folder - Split up files into specific file extension folders?
+mv *.gif Images/GIF
 mv *.jpeg Images/JPEG
 mv *.jpg Images/JPG
-mv *.gif Images/GIF
 mv *.png Images/PNG
 
 # Make folders for videos
 mkdir Videos
-mkdir Videos/MPEG
 mkdir Videos/AVI
 mkdir Videos/FLV
-mkdir Videos/WMV
-mkdir Videos/MOV
 mkdir Videos/MP4
+mkdir Videos/MPEG
+mkdir Videos/MOV
+mkdir Videos/WMV
 
 # Move all videos to folder - Split up files into specific file extension folders?
-mv *.MPEG Videos/MPEG
 mv *.AVI Videos/AVI
 mv *.FLV Videos/FLV
-mv *.WMV Videos/WMV
-mv *.MOV Videos/MOV
 mv *.MP4 Videos/MP4
+mv *.MPEG Videos/MPEG
+mv *.MOV Videos/MOV
+mv *.WMV Videos/WMV
 
 # misc
 mkdir Miscellaneous
