7edcf12b852eee8bc44cb9abc8a7baf307f477e0
JosephTLyons <josephlyons@josephs-air.pk5001z>
2017-01-20 04:14:43 -0500

    Added video support and reorganized

--- FileOrganizer.sh
+++ FileOrganizer.sh
@@ -2,6 +2,7 @@
 
 # TODO - use if-statements to make check if folder already exists
 # Add support for other files and formats
+# Use if statements before attemping to move anything - make sure they exist?
 
 # Make folders for documents
 mkdir Documents
@@ -10,6 +11,12 @@ mkdir Documents/RTF
 mkdir Documents/DOC
 mkdir Documents/HTML
 
+# Move all docs to Documents Folder
+mv *.txt Documents/TXT
+mv *.rtf Documents/RTF
+mv *.doc Documents/DOC
+mv *.html Documents/HTML
+
 # Make folders for images
 mkdir Images
 mkdir Images/JPEG
@@ -17,26 +24,31 @@ mkdir Images/JPG
 mkdir Images/GIF
 mkdir Images/PNG
 
-# Make folders for videos
-mkdir Videos
-mkdir Videos/MPEG
-# mkdir Videos/ OTHER TYPES
-
-mkdir Miscellaneous
-
-# Use if statements before attemping to move anything - make sure they exist
-
 # Move all Images to folder - Split up files into specific file extension folders?
 mv *.jpeg Images/JPEG
 mv *.jpg Images/JPG
 mv *.gif Images/GIF
 mv *.png Images/PNG
 
-# Move all docs to Documents Folder
-mv *.txt Documents/TXT
-mv *.rtf Documents/RTF
-mv *.doc Documents/DOC
-mv *.html Documents/HTML
+# Make folders for videos
+mkdir Videos
+mkdir Videos/MPEG
+mkdir Videos/AVI
+mkdir Videos/FLV
+mkdir Videos/WMV
+mkdir Videos/MOV
+mkdir Videos/MP4
+
+# Move all videos to folder - Split up files into specific file extension folders?
+mv *.MPEG Videos/MPEG
+mv *.AVI Videos/AVI
+mv *.FLV Videos/FLV
+mv *.WMV Videos/WMV
+mv *.MOV Videos/MOV
+mv *.MP4 Videos/MP4
+
+# misc
+mkdir Miscellaneous
 
 # Move rest of items to Miscellaneous Folder
 #mv *.* Miscellaneous
