8f4505aea34edc96881c557ceb76cedd603e84ec
JosephTLyons <josephlyons@josephs-air.pk5001z>
2017-01-23 04:33:37 -0500

    Working version, but need to rethink implementation so I can run it multiple times, can't do that right now as folders get regenerated and when attempting to move them, they can't be moved because folders of the same name already exist

--- FileOrganizer.sh
+++ FileOrganizer.sh
@@ -3,6 +3,8 @@
 # Figure out how to copy files from existing folders out and make them empty
 # so they can be sorted, and they will be deleted in the end
 
+# dont move this shell application
+
 ####################################################################################################
 
 # Get all file extensions and store in array, skip hidden files
@@ -17,11 +19,11 @@ for (( i = 0; i < itemsInArray; i++ ))
 do
 
     # Only move and make folders if its not a shell script
-    if [ ${fileArray[i]} -ne "sh" ];
-    then
+#    if [ ${fileArray[i]} -ne "sh" ];
+#    then
         mkdir ${fileArray[i]}
         mv *.${fileArray[i]} ${fileArray[i]}
-    fi
+#    fi
 done
 
 ####################################################################################################
@@ -36,8 +38,41 @@ mkdir Videos
 ####################################################################################################
 
 # Move all folder types to a category
-mv 
+mv aiff/ Audio/
+mv flac/ Audio/
+mv m4a/ Audio/
+mv mp3/ Audio/
+mv wav/ Audio/
 
+mv doc/ Documents/
+mv docx/ Documents/
+mv xlsx/ Documents/
+mv html/ Documents/
+mv md/ Documents/  # Read Me Files
+mv pdf/ Documents/
+mv pptx/ Documents/
+mv rtf/ Documents/
+mv sh/ Documents/  # Shell Scripts
+mv txt/ Documents/
+
+mv gif/ Images/
+mv jpeg/ Images/
+mv jpg/ Images/
+mv pxm/ Images/
+mv png/ Images/
+
+mv avi/ Videos/
+mv flv/ Videos/
+mv mp4/ Videos/
+mv mpeg/ Videos/
+mv mov/ Videos/
+mv wmv/ Videos/
+
+mv zip/ Miscellaneous/
+
+####################################################################################################
+
+ Delete all empty categories
 if [ "$(ls Audio 2> /dev/null)" == "" ];
 then
     rmdir Audio
@@ -51,6 +86,11 @@ fi
 if [ "$(ls Images 2> /dev/null)" == "" ];
 then
     rmdir Images
+fi
+
+if [ "$(ls Miscellaneous 2> /dev/null)" == "" ];
+then
+    rmdir Miscellaneous
 fi
 
 if [ "$(ls Videos 2> /dev/null)" == "" ];
